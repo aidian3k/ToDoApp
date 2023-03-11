22fe54bddacdd1567baca1407cf9ea26fcd10f9f
aidian3k <adrian.nowosielski1@gmail.com>
2023-03-11 14:57:55 +0100

    Added service

---
+++ backend/src/main/java/todo/backend/project/service/TaskService.java
@@ -0,0 +1,38 @@
+package todo.backend.project.service;
+
+import org.springframework.stereotype.Service;
+import todo.backend.project.entity.Task;
+import todo.backend.project.repository.TaskRepository;
+import todo.backend.project.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository) {
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+    }
+    public List<Task> getAllTasks() {
+        return (List<Task>) taskRepository.findAll();
+    }
+
+    public Task createNewTask(Task task) {
+        taskRepository.save(task);
+
+        return task;
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Task updateTask(Task task) {
+        taskRepository.save(task);
+
+        return task;
+    }
+}
